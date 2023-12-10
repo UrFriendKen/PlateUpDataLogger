e70928923206ff67292e82f93e5a301d28bf3f3a
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-12-10 14:15:07 +0800

    Added patch for LayoutFailureException message

---
+++ Patches/LayoutFailureException_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Kitchen.Layouts;
+using System;
+
+namespace KitchenDataLogger.Patches
+{
+    [HarmonyPatch]
+    static class LayoutFailureException_Patch
+    {
+        [HarmonyPatch(typeof(LayoutFailureException), MethodType.Constructor, new Type[] { typeof(string) })]
+        [HarmonyPrefix]
+        static void Ctor_Prefix(string message)
+        {
+            Main.LogError(message);
+        }
+    }
+}
